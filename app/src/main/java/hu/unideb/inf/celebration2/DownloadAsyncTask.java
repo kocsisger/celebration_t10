0698cf6eadb931dcf9354f580a7a2dd0fb02de48
Kocsis Gergely <kocsisger@gmail.com>
2026-04-28 11:04:51 +0200

    Sleep

--- app/src/main/java/hu/unideb/inf/celebration2/DownloadAsyncTask.java
+++ app/src/main/java/hu/unideb/inf/celebration2/DownloadAsyncTask.java
@@ -1,4 +1,40 @@
 package hu.unideb.inf.celebration2;
 
-public class DownloadAsyncTask {
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class DownloadAsyncTask extends AsyncTask<Void, Integer, String> {
+
+    WeakReference<Button> weakButton;
+    WeakReference<ProgressBar> weakProgressBar;
+    WeakReference<TextView> weakTextView;
+
+    public DownloadAsyncTask(Button button, ProgressBar progressBar, TextView textView) {
+        this.weakButton = new WeakReference<>(button);
+        this.weakProgressBar = new WeakReference<>(progressBar);
+        this.weakTextView = new WeakReference<>(textView);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            for (int i = 0; i < 100; i++) {
+                Thread.sleep(10);
+                publishProgress(i);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        weakProgressBar.get().setProgress(values[0]);
+    }
 }
